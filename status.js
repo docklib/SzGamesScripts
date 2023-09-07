fc56cd147c3b168660ed174cb0145296df4b5d44
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-09-07 08:09:17 -0700

    Update status.js

--- status.js
+++ status.js
@@ -1,5 +1,5 @@
-var status = 'online'
-var problem = false
-var issueName = 'test'
-var statustxt = 'Test Issue'
-var statusTime = 'Last Updated: 10PM PST'
+var status = 'error'
+var problem = true
+var issueName = 'Games May Load Slow'
+var statustxt = 'We are working to fix this issue.'
+var statusTime = 'Last Updated: 8AM PST'
