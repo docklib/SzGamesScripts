d497f64500d0683a4ed6d31ba0526d46890f898b
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-09-11 22:12:34 -0700

    Update status.js

--- status.js
+++ status.js
@@ -1,5 +1,5 @@
-var status = 'error'
+var status = 'online'
 var problem = true
 var issueName = 'Homepage Loading Slow'
-var statustxt = 'We are working to fix this.'
+var statustxt = 'This issue was fixed.'
 var statusTime = 'Last Updated: Sep. 8th, 10AM PST'
