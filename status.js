1798571ebacb9f346c8d859113cb12b31ef09dd3
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-12-23 13:22:59 -0800

    Add files via upload

--- status.js
+++ status.js
@@ -1,5 +1,5 @@
-var status = 'online'
+var status = 'error'
 var problem = true
-var issueName = 'Homepage Loading Slow'
-var statustxt = 'This issue was fixed.'
-var statusTime = 'Last Updated: Sep. 8th, 10AM PST'
+var issueName = 'Some Games Are Not Available'
+var statustxt = 'Due To Issues Some Games Such As FNAF Are Not Currently Available, We dont currently know when the games will be restored.'
+var statusTime = 'Last Updated: Dec. 23rd, 1PM PST'
